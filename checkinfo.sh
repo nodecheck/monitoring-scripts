12d4b9b1332ef7567ca80c8c09f60ad118178913
NodeCheck <43926338+nodecheck@users.noreply.github.com>
2018-12-20 16:05:42 +0100

    Initial script v1.0
    NodeCheck Monitoring Script for added functionality.

---
+++ checkinfo.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+##################################
+# Copyright: 2018 - Nodecheck.io #
+# Version: 1.0                   #
+##################################
+
+######################################################################
+# Script to check and verify wallet version for NodeCheck.io MN      #
+# Monitoring Platform. This will then allow for providing            #
+# notifications when a new wallet is available for the MN you are    #
+# monitoring.                                                        #
+######################################################################
+RUNMODE=$1
+
+##################
+# Output to file #
+##################
+# File to send output to before sending to NodeCheck
+OUTPUT=/tmp/checkinfo.json
+
+######################################################################
+# The following four variables need to be set:                       #
+#                                                                    #
+# 1. Replace CLITOOL with path to cli tool. An example is listed.    #
+# 2. Add your API KEY from NodeCheck website.                        #
+# 3. Add TXID of your masternode (from MN config file).              #
+# 4. Add payee/pubkey of your masternode (from MN config file).      #
+######################################################################
+CLITOOL=/path/to/coin-cli|coindaemond
+APIKEY=<your-api-key-here>
+TXID=<your-mn-txid-here>
+PAYEE=<your-mn-payee-here>
+
+############################
+# Info to check and update #
+############################
+# Get version, blocks, and blockhash
+if [[ $CLITOOL == *"-cli" ]]
+then
+	# Coin uses coin-cli command
+	VER=`$CLITOOL getnetworkinfo | grep subversion | cut -f4 -d "\""`
+	# Remove / character from version info
+	VERSION=`echo $VER | sed 's/\///g'`
+else
+	# Coin uses coind daemon command
+	VER=`$CLITOOL getinfo | grep version | egrep -iv "protocol|wallet" | cut -f2 -d "="`
+        # Remove / character from version info
+        VERSION=`echo $VER | sed 's/\///g'`
+fi
+
+BLOCKS=`$CLITOOL getblockcount`
+BLOCKHASH=`$CLITOOL getblockhash $BLOCKS`
+
+#####################################################################
+# Update users' MN info on NodeCheck.io using API to update:        #
+# VERSION/BLOCKS/BLOCKHASH                                          #
+#####################################################################
+# Send output to file
+echo "{\"access-token\":\"$APIKEY\", \"payee\":\"$PAYEE\", \"txid\":\"$TXID\", \"version\":\"$VERSION\", \"blocks\":\"$BLOCKS\", \"blockhash\":\"$BLOCKHASH\"}" > $OUTPUT
+
+# Check how we ran script
+# If no parameter passed, then run normally
+if [ -z "$RUNMODE" ]
+then
+	# Update user's MN info no NodeCheck
+	sleep $[ ( $RANDOM % 600 ) ]
+        RESULTS=`curl -s -d @$OUTPUT -H "Content-Type: application/json" https://test.nodecheck.io/api/sendinfo`
+	# Check if successful or not and display error
+	if [[ $RESULTS == *"\"success\":true"* ]]
+	then
+		# All working OK
+		exit 0
+	else
+		# There seems to be a problem!
+		echo "Error: $RESULTS"
+		exit 1
+	fi
+else
+	# Check if we used --test parameter
+	if [ $RUNMODE == "--test" ]
+	then
+		# Display test results to verify script configuration
+		echo "Test to verify if script is working."
+		echo ""
+		echo "Information being submitted:"
+		echo ""
+		echo "MN/Wallet Version=$VERSION"
+		echo "Blockheight=$BLOCKS"
+		echo "Blockhash=$BLOCKHASH"
+		echo ""
+		exit 0
+	else
+		# Wrong parameter provided, display help information
+		echo "Incorrect parameter provided to script."
+		echo ""
+		echo "Usage: checkinfo.sh [--test]"
+		echo ""
+		exit 1
+	fi
+fi
